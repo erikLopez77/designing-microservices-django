3874334003b0414d2d8a324e8c7c0badbd7f1075
ErikLopez <espinozalopezerik@gmail.com>
2026-09-01 12:48:12 -0600

    feat: modelo iniicial, falta aplicar cambios en la db

--- tizza/pizza/models.py
+++ tizza/pizza/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Pizza(models.Model):
     title=models.CharField(max_length=120)
     description=models.CharField(max_length=240)
-
+    size=models.CharField(max_length=30)
