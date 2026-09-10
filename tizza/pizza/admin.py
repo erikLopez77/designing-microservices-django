925e3571dc9e89938be7331420b6b3d1d865bbda
ErikLopez <espinozalopezerik@gmail.com>
2026-09-10 17:17:23 -0600

    feat: vista y urls

--- tizza/pizza/admin.py
+++ tizza/pizza/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Pizza
+
+class PizzaAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Pizza, PizzaAdmin)
--- tizza/pizza/views.py
+++ tizza/pizza/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Pizza
+from django.http import HttpResponse,JsonResponse
 # Create your views here.
+def index(request,pid):
+    try:
+        pizza=Pizza.objects.get(id=pid)
+
+        return JsonResponse({
+            'id':pizza.id,
+            'title':pizza.title,
+            'description':pizza.description
+        })
+
+    except Pizza.DoesNotExist:
+        return JsonResponse({
+            "status":"error",
+            "message":"pizza not found"
+        })
